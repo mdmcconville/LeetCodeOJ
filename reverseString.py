ea53b3c20ec1bbfbef4cf1bc683c1d49dca9584d
Mallory McConville <mallory@Mallorys-MacBook-Pro.local>
2014-12-13 17:35:29 -0600

    improved

--- reverseString.py
+++ reverseString.py
@@ -6,8 +6,6 @@ an input string. For example, "See Spot run" would be "run Spot See" reversed.
 """
 
 def reverseIt(string):
-    # Make the string into a list
-    lst = string.split()
-    lst.reverse()
-    # Make the reversed list into a string
-    return ' '.join(lst)
+    # Make the string into a list, reverse it, and back to string
+    return ' '.join(reversed(string.split()))
+
